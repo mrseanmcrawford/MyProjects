93646ffb476f477fdc865a8bd18a8bbbbd890bd1
mrseanmcrawford <Mrseanmcrawford@icloud.com>
2021-02-17 21:01:18 -0800

    updating multiple files

---
+++ Coding Bootcamp/JavaScript/Basic JavaScript Projects/Project_expressions_alert/JS/1Project.js
@@ -0,0 +1,8 @@
+window.alert ("Hello, my friend") //Window pop up   
+var A = ("Test, 1, 2, 3")         //Assigning Variable 
+var B = (" Test A, B, C")         //Assigning Variable 
+document.write (A+B)              //Printing both Variables
+var blues = " I have the blues."  // Assingining Variable 
+var blues = blues.fontcolor ("blue") //Assigning color to Variable
+document.write(blues)              //Printing Variable 
+document.write(8+8)                //Writing and prtinting an expression
---
+++ Coding Bootcamp/JavaScript/Basic JavaScript Projects/Project_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+function My_First_Function () {                 //Defining a function and naming it
+    var str = "This text is green!";            //Defining a variable and giving it a string value
+    var result = str.fontcolor("green");        //Using the fontcolor method on str variable
+    document.getElementById("Green_Text").innerHTML = result; //Put
+}
