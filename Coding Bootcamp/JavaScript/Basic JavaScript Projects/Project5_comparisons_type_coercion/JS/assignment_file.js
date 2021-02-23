f77f7e7401e1272835d3468f18e4e16f25bd036a
mrseanmcrawford <Mrseanmcrawford@icloud.com>
2021-02-22 19:51:38 -0800

    update assignment

--- Coding Bootcamp/JavaScript/Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/assignment_file.js
+++ Coding Bootcamp/JavaScript/Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/assignment_file.js
@@ -1,15 +1,21 @@
 document.write(typeof 45) //Utilize the document.write() method and typeof operator to display the data type of a variable of your choosing
+document.write("<br />")  //Line break
 document.write("10" + 5); //Write an expression combining a string and a number 
+document.write("<br />")  //Line break
 document.write(10 == 10); //Write code utilizing the following operators: ==, ===, >, <, &&, ||, ! and display them in the browser
-
+document.write("<br />")  //Line break
 Y = 10;
 X = 10;
 document.write( X === Y);
+document.write("<br />")  //Line break
 
 document.write(5 > 2 || 10 > 4); 
+document.write("<br />")  //Line break
 document.write(5 > 10 || 10 > 20);
-
+document.write("<br />")  //Line break
 document.write(5 > 2 && 10 > 4); 
+document.write("<br />")  //Line break
 document.write(5 > 10 && 10 > 20);
+
 
 
