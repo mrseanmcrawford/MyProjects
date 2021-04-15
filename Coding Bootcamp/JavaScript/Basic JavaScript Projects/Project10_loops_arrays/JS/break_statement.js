aad0b1ef80a0e02d7c9d7e198808568cccd88dc1
mrseanmcrawford <Mrseanmcrawford@icloud.com>
2021-04-15 05:35:18 -0700

    update calcuklator files

--- Coding Bootcamp/JavaScript/Basic JavaScript Projects/Project10_loops_arrays/JS/break_statement.js
+++ Coding Bootcamp/JavaScript/Basic JavaScript Projects/Project10_loops_arrays/JS/break_statement.js
@@ -1,7 +1,7 @@
-var text = "";
-var i;
-for (i = 0; i < 10; i++) {
-    if (i === 3) { break; }
-    text += "The number is " + i + "<br>";
+var text = "";                                                    //Defining Variable//
+var i;                                                           //Defining Variable//
+for (i = 0; i < 10; i++) {                                       //Defining Function//
+    if (i === 3) { break; }                                      //Defining Function//
+    text += "The number is " + i + "<br>";                       //Defining Function//
 }
 document.getElementById("demo").innerHTML = text;
--- Coding Bootcamp/JavaScript/Basic JavaScript Projects/Project10_loops_arrays/JS/continue_statement.js
+++ Coding Bootcamp/JavaScript/Basic JavaScript Projects/Project10_loops_arrays/JS/continue_statement.js
@@ -1,7 +1,7 @@
-var text = "";
-var i;
-for (i = 0; i < 10; i++) {
-    if (i == 3) { continue; }
-    text += "the number is " + i + "<br>";
+var text = "";                                                  //Defining Variable//
+var i;                                                          //Defining Variable//
+for (i = 0; i < 10; i++) {                                      //Defining function//
+    if (i == 3) { continue; }                                   //Defining function//
+    text += "the number is " + i + "<br>";                      //Defining function//
 }
-document.getElementById("demo").innerHTML = text;
+document.getElementById("demo").innerHTML = text;           
