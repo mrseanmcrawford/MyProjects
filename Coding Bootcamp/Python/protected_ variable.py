46ca60658eeba267e60d1030b19e0b7b5715e98b
mrseanmcrawford <Mrseanmcrawford@icloud.com>
2021-07-26 21:40:42 -0700

    Update private and protected variables files

---
+++ Coding Bootcamp/Python/protected_ variable.py
@@ -0,0 +1,104 @@
+Python 3.9.5 (v3.9.5:0a7dcbdb13, May  3 2021, 13:17:02) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py =
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py", line 11, in <module>
+    from.tkinter import *
+ImportError: attempted relative import with no known parent package
+>>> 
+== RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py =
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py", line 11, in <module>
+    from.tkinter import *
+ImportError: attempted relative import with no known parent package
+>>> 
+== RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py =
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py", line 16, in <module>
+    import phonebook_gui
+ModuleNotFoundError: No module named 'phonebook_gui'
+>>> 
+== RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py =
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py", line 16, in <module>
+    import phonebook_gui
+ModuleNotFoundError: No module named 'phonebook_gui'
+>>> 
+= RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook_gui.py
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook_gui.py", line 6, in <module>
+    import phonebook_main
+ModuleNotFoundError: No module named 'phonebook_main'
+>>> 
+== RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py =
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py", line 16, in <module>
+    import phonebook_gui
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook_gui.py", line 34
+    self.1stList1 = Listbox(self.master,exportselection=0,yscrollcommand=self.scrollbar1.set)
+        ^
+SyntaxError: invalid syntax
+>>> 
+== RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py =
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py", line 16, in <module>
+    import phonebook_gui
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook_gui.py", line 34
+    self.1stList1 = Listbox(self.master,exportselection=0,yscrollcommand=self.scrollbar1.set)
+        ^
+SyntaxError: invalid syntax
+>>> 
+== RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py =
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook.py", line 16, in <module>
+    import phonebook_gui.py
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/phonebook_gui.py", line 34
+    self.1stList1 = Listbox(self.master,exportselection=0,yscrollcommand=self.scrollbar1.set)
+        ^
+SyntaxError: invalid syntax
+>>> 
+= RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/Pythonencapsulate.py
+34
+>>> 
+= RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/Phonebook App/phonebook_main.py
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/Phonebook App/phonebook_main.py", line 15, in <module>
+    import phonebook_gui
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/Phonebook App/phonebook_gui.py", line 34
+    self.1stList1 = Listbox(self.master,exportselection=0,yscrollcommand=self.scrollbar1.set)
+        ^
+SyntaxError: invalid syntax
+>>> 
+
+= RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/Phonebook App/phonebook_main.py
+================================ RESTART: Shell ================================
+>>> 
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/Phonebook App/phonebook_main.py", line 15, in <module>
+    import phonebook_gui
+ModuleNotFoundError: No module named 'phonebook_gui'
+>>> import phonebook_gui
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    import phonebook_gui
+ModuleNotFoundError: No module named 'phonebook_gui'
+>>> import phonebook_gui.py
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    import phonebook_gui.py
+ModuleNotFoundError: No module named 'phonebook_gui'
+>>> 
+= RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/Phonebook App/phonebook_main.py
+Traceback (most recent call last):
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/Phonebook App/phonebook_main.py", line 15, in <module>
+    import phonebook_gui
+  File "/Users/seancrawford/MyProjects/Coding Bootcamp/Python/Phonebook App/phonebook_gui.py", line 34
+    self.1stList1 = Listbox(self.master,exportselection=0,yscrollcommand=self.scrollbar1.set)
+        ^
+SyntaxError: invalid syntax
+>>> 
+= RESTART: /Users/seancrawford/MyProjects/Coding Bootcamp/Python/Pythonencapsulate.py
+75
+>>> 
